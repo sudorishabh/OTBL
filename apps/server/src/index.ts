057adb76253996530a0549d2653646e436d14170
Rishabh Negi <rishabhnegi175@gmail.com>
2026-03-16 12:40:10 +0530

    merge server index.ts and app.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,34 @@
+import cookieParser from "cookie-parser";
+import express, { json, response, urlencoded, Request, Response } from "express";
+import cors from "./config/cors";
+import * as trpcExpress from "@trpc/server/adapters/express";
+// import { appRouter } from "./trpc/router";
+import { appRouter } from "@pkg/trpc";
+import { createContext } from "./context";
+import { errorHandler } from "./middlewares/error-handler";
 import "dotenv/config";
-import app from "./app";
+
+const app = express();
+
+app.use(cors);
+app.use(json({ limit: "100mb" }));
+app.use(urlencoded({ extended: true, limit: "100mb" }));
+app.use(cookieParser());
+
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: createContext,
+  })
+);
+
+app.use(errorHandler);
 
 app.listen(7200, () => {
   console.log(`Server is running on port 7200 in ${process.env.NODE_ENV} mode`);
 });
+
+
+export default app;
